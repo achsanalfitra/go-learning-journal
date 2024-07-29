a537ed9f5f11253d6d3906e128535e5ab5fc8dda
achsanalfitra <achsanalfitra@gmail.com>
2024-07-29 17:55:56 +0700

    modify main file

---
+++ cmd/exec/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/achsanalfitra/go-learning-journal/basic-go-syntax/controls"
+)
+
+func main() {
+	i := []int{2, 1, 3, 4, 6, 5, 7}
+
+	controls.BubbleSort(&i)
+	fmt.Println(i)
+}
